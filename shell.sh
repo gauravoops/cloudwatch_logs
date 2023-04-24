e2f9fe776b258ac0dd6690aee83953a3453f2977
gauravoops <124277195+gauravoops@users.noreply.github.com>
2023-04-24 14:17:51 +0530

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i amazon-cloudwatch-agent.deb
+sudo mv config.json /opt/aws/amazon-cloudwatch-agent/bin/
+
+sudo apt-get update -y
+sudo apt-get install collectd
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status 
